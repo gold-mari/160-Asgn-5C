c105eddf6f249db87ca829ae72b0a3a32cafac61
Dylan Mahler <dmahler@ucsc.edu>
2024-04-21 18:09:47 -0700

    TWO MORE CUBES!!!

--- main.js
+++ main.js
@@ -15,13 +15,11 @@ function main() {
     const scene = new THREE.Scene();
 
     {
-
         const color = 0xFFFFFF;
         const intensity = 3;
         const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(- 1, 2, 4);
+        light.position.set(-1, 2, 4);
         scene.add(light);
-
     }
 
     const boxWidth = 1;
@@ -31,20 +29,39 @@ function main() {
 
     const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const cubes = [
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0x44aa88, 0),
+        makeInstance(geometry, 0xaa8844, 2),
+    ];
 
     function render(time) {
 
         time *= 0.001; // convert time to seconds
 
-        cube.rotation.x = time;
-        cube.rotation.y = time;
-
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * 0.1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
 
         renderer.render(scene, camera);
 
         requestAnimationFrame(render);
+
+    }
+
+    function makeInstance(geometry, color, x) {
+
+        const material = new THREE.MeshPhongMaterial({color});
+    
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+    
+        cube.position.x = x;
+    
+        return cube;
 
     }
 
