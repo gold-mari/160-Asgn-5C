7936c5c753fb939ffa7d271ebff0503b9ea77920
Dylan Mahler <dmahler@ucsc.edu>
2024-04-21 23:49:32 -0700

    Implemented textures

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ import * as THREE from 'three';
 
 function main() {
 
+    // Set up the canvas and renderer
+    // ================================
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
+    // Set up the camera
+    // ================================
     const fov = 75;
     const aspect = 2; // the canvas default
     const near = 0.1;
@@ -12,6 +16,8 @@ function main() {
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2;
 
+    // Set up the scene and lights
+    // ================================
     const scene = new THREE.Scene();
 
     {
@@ -22,17 +28,42 @@ function main() {
         scene.add(light);
     }
 
+    // Set up the box prefab
+    // ================================
+    const cubes = [];
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
+    // Set up asset loading
+    // ================================
+    const loadManager = new THREE.LoadingManager();
+    const loader = new THREE.TextureLoader(loadManager);
+    
+    const loadingElem = document.querySelector('#loading');
+    const progressBarElem = loadingElem.querySelector('.progressbar');
 
-    const cubes = [
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0x44aa88, 0),
-        makeInstance(geometry, 0xaa8844, 2),
+    loadManager.onLoad = () => {
+        loadingElem.style.display = 'none';
+
+        makeInstance(geometry, -2);
+        makeInstance(geometry, 0);
+        makeInstance(geometry, 2);
+    }
+
+    loadManager.onProgress = (urlOfLastItemLoaded, itemsLoaded, itemsTotal) => {
+        const progress = itemsLoaded / itemsTotal;
+        progressBarElem.style.transform = `scaleX(${progress})`;
+    };
+
+    const materials = [
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-1.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-2.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-3.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-4.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-5.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('resources/images/flower-6.jpg')}),
     ];
 
     function render(time) {
@@ -59,24 +90,37 @@ function main() {
 
     }
 
-    function makeInstance(geometry, color, x) {
+    function makeInstance(geometry, x) {
 
-        const material = new THREE.MeshPhongMaterial({color});
-    
-        const cube = new THREE.Mesh(geometry, material);
+        const cube = new THREE.Mesh(geometry, materials);
         scene.add(cube);
-    
+        cubes.push(cube);
+
         cube.position.x = x;
-    
+
         return cube;
 
     }
 
     requestAnimationFrame(render);
 
+    // ================
+    // Helper functions
+    // ================
+
+    function loadColorTexture(path) {
+        const texture = loader.load(path);
+        texture.colorSpace = THREE.SRGBColorSpace;
+        return texture;
+    }
+
 }
 
 main();
+
+// ================
+// Misc
+// ================
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
